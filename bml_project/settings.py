0b0b1f92d122be2a1a8443c9fdecbd2fcbb17e2a
ChrisJiang <jzcmelody@gmail.com>
2024-07-26 22:47:47 +0900

    migrate db

--- bml_project/settings.py
+++ bml_project/settings.py
@@ -1,7 +1,5 @@
 from pathlib import Path
 
-
-ROOT_URLCONF = "blogmaker_lite"
 DEBUG = True
 SECRET_KEY = "my-secret-key"
 
@@ -11,3 +9,14 @@ TEMPLATES = [
         "DIRS": [Path(__file__).parent / "templates"],
     }
 ]
+
+INSTALLED_APPS = ["blogs"]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": Path(__file__).parent / "db.sqlite3",
+    }
+}
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
