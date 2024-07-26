87b7e68f491fb5956b5f585e52dd8739e3f56bde
ChrisJiang <jzcmelody@gmail.com>
2024-07-27 00:14:12 +0900

    register blog app to admin

--- bml_project/blogmaker_lite.py
+++ bml_project/blogmaker_lite.py
@@ -1,13 +1,17 @@
+from blogs.models import Blog
+from django.contrib import admin
 from django.core.handlers.wsgi import WSGIHandler
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.urls import path
+
+admin.site.register(Blog)
 
 
 def index(request) -> HttpResponse:
     return render(request, "index.html")
 
 
-urlpatterns = [path("", index)]
+urlpatterns = [path("admin/", admin.site.urls), path("", index)]
 
 application = WSGIHandler()
