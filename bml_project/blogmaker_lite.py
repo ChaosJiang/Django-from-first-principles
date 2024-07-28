29f095fa5934d42dfd5ac3cc8d11563407409f00
ChrisJiang <jzcmelody@gmail.com>
2024-07-28 23:03:45 +0900

    refactor

--- bml_project/blogmaker_lite.py
+++ bml_project/blogmaker_lite.py
@@ -1,44 +1,9 @@
-from blogs.models import Blog, BlogPost
 from django.contrib import admin
 from django.core.handlers.wsgi import WSGIHandler
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.urls import path
-
-admin.site.register((Blog, BlogPost))
-
-
-def index(request) -> HttpResponse:
-    return render(request, "index.html")
-
-
-def blogs(request) -> HttpResponse:
-    all_blogs = Blog.objects.all()
-    context = {"blogs": all_blogs}
-    return render(request, "blogs.html", context)
-
-
-def blog(request, blog_id) -> HttpResponse:
-    blog = Blog.objects.get(id=blog_id)
-    posts = blog.blogpost_set.all()
-    context = {"blog": blog, "posts": posts}
-    return render(request, "blog.html", context)
-
-
-def post(request, post_id) -> HttpResponse:
-    post = BlogPost.objects.get(id=post_id)
-    blog = post.blog
-    context = {"post": post, "blog": blog}
-
-    return render(request, "post.html", context)
-
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("blogs/", blogs, name="blogs"),
-    path("blogs/<int:blog_id>/", blog, name="blog"),
-    path("posts/<int:post_id>/", post, name="post"),
-    path("", index, name="index"),
+    path("", include("blogs.urls")),
 ]
-
 application = WSGIHandler()
---
+++ bml_project/blogs/admin.py
@@ -0,0 +1,4 @@
+from blogs.models import Blog, BlogPost
+from django.contrib import admin
+
+admin.site.register((Blog, BlogPost))
